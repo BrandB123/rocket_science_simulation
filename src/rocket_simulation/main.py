f608d05cf4d3c0823b4e374d554ffddcd3124eff
Brandon Bartlett <153580357+BrandB123@users.noreply.github.com>
2026-05-25 18:46:40 -0500

    feat: call basic flight simulation in main

--- src/rocket_simulation/main.py
+++ src/rocket_simulation/main.py
@@ -1,5 +1,7 @@
 from src.rocket_simulation.vehicles.motors import Motor
 from src.rocket_simulation.vehicles.rockets import Rocket
+from src.rocket_simulation.simulation.launch import basic_flight_simulation
+
 import json
 from pathlib import Path
 
@@ -17,8 +19,46 @@ def main():
     rocket = Rocket(**rocket_data)
     motor = Motor(**motor_data)
 
-    print(f"Rocket: {rocket.name}")
-    
-    print(f"Motor: {motor.name}")
+    print(
+        f"""
+        ---------------------------------------
+
+                  FLIGHT SIMULATION:
+
+        ---------------------------------------
+
+                  ROCKET: {rocket.name}
+                  MOTOR:  {motor.name}
+
+        ---------------------------------------
+        """
+    )
+
+    try:
+        
+        print("           RESULTS: ")
+        basic_flight_simulation(rocket, motor)
+        print(""" 
+        --------------------------------------- 
+              
+            FLIGHT SIMULATION COMPLETED
+              
+        --------------------------------------- 
+        """)
+    except TypeError as e:
+        print(f"Type Error: {e}")
+    except ValueError as e:
+        print(f"Value Error: {e}")
+
+
+    # implemnt function for flight results with drag and call it here
+    #   for each time slice
+    #       calculate drag ( drag = 1/2 * air density * drag coefficient * frontal cross-sectional area * velocity^2 )
+    #       calculate forces at the current instant
+    #       compute accelaration
+    #       update velocity
+    #       update altitude
+    #       advance time
+    #       repeat
 
 main()
