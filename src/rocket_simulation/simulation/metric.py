f4be5e3332005d73fb0cf9bc567d4f7d54663aa7
Brandon Bartlett <153580357+BrandB123@users.noreply.github.com>
2026-05-27 00:00:44 -0500

    feat: add methods for updating and logging metrics

--- src/rocket_simulation/simulation/metric.py
+++ src/rocket_simulation/simulation/metric.py
@@ -1,4 +1,5 @@
-from dataclasses import dataclass
+from dataclasses import asdict, dataclass
+from src.rocket_simulation.physics.constants import FEET_PER_METER
 
 @dataclass
 class Metrics:
@@ -7,10 +8,10 @@ class Metrics:
     apogee_ft: float = 0.0
     burnout_altitude_m: float = 0.0
     burnout_velocity_m_s: float = 0.0
-    burn_time_s: float = 0.0
+    burnout_time_s: float = 0.0
 
     # motor stats
-    total_impulse_ns: float = 0.0
+    total_impulse_ns: float = 0.0           
     average_thrust_n: float = 0.0
     max_thrust_n: float = 0.0
 
@@ -25,5 +26,46 @@ class Metrics:
 
     # TODO: add max Q, dynamic pressure curve, terminal velocity
 
-    # TODO: add a method to listen for given set of events based on the 
-    # flight state object to set the above metrics at the time time
+    def update(self, end_sim_condition: bool, state):
+        
+        if not isinstance(end_sim_condition, bool):
+            raise TypeError("end_sim_condition must be a boolean")
+        
+        if state.acceleration <= 0 and not self.burnout_time_s:
+            self.burnout_altitude_m = state.altitude
+            self.burnout_velocity_m_s = state.velocity
+            self.burnout_time_s = state.time
+
+        if state.velocity <= 0 and not self.reached_apogee:
+            self.apogee_m = state.altitude
+            self.apogee_ft = state.altitude * FEET_PER_METER
+            self.reached_apogee = True
+
+        if self.max_velocity_m_s < state.velocity:
+            self.max_velocity_m_s = state.velocity
+        
+        if self.max_acceleration_m_s2 < state.acceleration:
+            self.max_acceleration_m_s2 = state.acceleration
+
+        if end_sim_condition:
+            self.flight_duration_s = state.time
+    
+    def log(self):
+        print("\nMETRICS:")
+
+        print("\nBurnout:")
+        print(f" - Altitude @ Burnout: {self.burnout_altitude_m:.2f} m")
+        print(f" - Velocity @ Burnout: {self.burnout_velocity_m_s:.2f} m/s")
+        print(f" - Time @ Burnout: {self.burnout_time_s:.2f} s")
+
+        print("\nMaximums:")
+        print(f" - Max Velocity: {self.max_velocity_m_s:.2f} m/s")
+        print(f" - Max Acceleration: {self.max_acceleration_m_s2:.2f} m/s²")
+
+        print(f"\nApogee: {self.apogee_m:.2f} m ({self.apogee_ft:.2f} ft)")
+        print(f"Time: {self.flight_duration_s:.2f} s")
+
+        
+
+        
+        
